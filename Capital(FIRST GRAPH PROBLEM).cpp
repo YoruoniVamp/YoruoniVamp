7418ad8efbaff8e6a1740456ca11888f3d6b23e7
YoruoniVamp <Peerawedtheerak@gmail.com>
2024-11-25 01:53:16 +0700

    My first graph problems
    I've learned depth first search algorithm graph today, and it's my first time I know about graph in informatics problem.
    The problem is about to find a longest distance from city number 1 to the furthest city.
    This is an example of the graph and dfs. C++ code
    
    YoruoniVamp - 24/11/2567

---
+++ Capital(FIRST GRAPH PROBLEM).cpp
@@ -0,0 +1,40 @@
+// YT - YoruoniVamp (VTube)
+
+#include <bits/stdc++.h>
+using namespace std;
+#define endl "\n"
+#define ll long long
+#define ld lond double
+#define ull unsigned long long
+#define cint const int
+#define cf const float
+
+cint mxA = 1e5+5;
+vector<pair<int,int>> gph[mxA];
+bool vis[mxA];
+int n, u, v, w;
+
+int dfs(int a){
+    int ans = 0;
+    vis[a] = true;
+    for(auto [v,w]: gph[a]){
+        if(vis[v]) continue;
+        ans = max(ans, w+dfs(v));
+    }return ans;
+}
+
+void solve(){
+    cin >> n;
+    n--;
+    while(n--){
+        cin >> u >> v >> w;
+        gph[v].push_back({u,w});
+        gph[u].push_back({v,w});
+    }cout << dfs(1);
+}
+
+int main(){
+    cin.tie(nullptr)->sync_with_stdio(0);cout.tie(0);
+    solve();
+    return 0;
+}
